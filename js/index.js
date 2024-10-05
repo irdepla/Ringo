b100e69affee90512b52cccffad5d0dc05e9a09b
irdepla <rixsiboyevislombek@gmail.com>
2024-10-05 22:51:31 +0500

    fix: mobile

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+const closeBtn = document.getElementById("close-btn");
+
+// Add event listener for closing the modal via the close button
+closeBtn.addEventListener("click", () => {
+  navModal.classList.remove("nav__modal--active");
+  overlay.classList.remove("nav__overlay--active");
+  burgerBtn.classList.remove("active"); // Reset the burger button state as well
+});
+
+
 const burgerBtn = document.getElementById("burger-btn");
 const navModal = document.getElementById("nav-modal");
 const overlay = document.querySelector(".nav__overlay");
