50acf2c489835ac6c2aaa043a7eab5483ad5a252
Toirjon-khaydarov <khaydarov_0507@mail.ru>
2024-10-06 17:45:00 +0500

    3-person vazifasini tugatdim

--- js/index.js
+++ js/index.js
@@ -1,13 +1,1 @@
-const burgerBtn = document.getElementById("burger-btn");
-const navModal = document.getElementById("nav-modal");
-const overlay = document.querySelector(".nav__overlay");
 
-burgerBtn.addEventListener("click", () => {
-  navModal.classList.toggle("nav__modal--active");
-  overlay.classList.toggle("nav__overlay--active");
-});
-
-overlay.addEventListener("click", () => {
-  navModal.classList.remove("nav__modal--active");
-  overlay.classList.remove("nav__overlay--active");
-});
